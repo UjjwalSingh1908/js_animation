eb8094f67c5bbcdd35e3b6795644de10583b465c
UjjwalSingh1908 <ujjwal1911044@akgec.ac.in>
2020-08-26 01:23:28 +0530

    added the boat animation

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function moveBackground(){
+   
+  }
+
+  
+
+
+
+    
+var boat = document.querySelector("#boat");
+  setTimeout(function() {
+    boat.style.transform = "translateX(1550px)";
+  }, 200);
+
