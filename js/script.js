c31f9e980b52f494582f1d44e3035a215db16719
UjjwalSingh1908 <ujjwal1911044@akgec.ac.in>
2020-08-26 02:19:37 +0530

    added the lotus and background animation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-function moveBackground(){
-   
+  function moveBackground(){
+    let pos = 0;
+    let bg = document.getElementById("water");
+    let fxnId = setInterval(bgmove, 1);
+    function  bgmove(){
+      pos += 100;
+      bg.style.backgroundPositionX = pos + "px";
+    }
   }
-
   
 
-
-
-    
 var boat = document.querySelector("#boat");
   setTimeout(function() {
     boat.style.transform = "translateX(1550px)";
   }, 200);
 
+  var lotus1 = document.querySelector("#lotus1");
+  setTimeout(function() {
+    lotus1.style.transform = "translateX(-1500px)";
+  }, 200);
+
+  var lotus2 = document.querySelector("#lotus2");
+  setTimeout(function() {
+    lotus2.style.transform = "translateX(-1200px)";
+  }, 200);
