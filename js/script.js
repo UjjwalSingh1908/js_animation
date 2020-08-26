dc86cceb84ee07085777b34ecdfad72f44a5c71d
UjjwalSingh1908 <ujjwal1911044@akgec.ac.in>
2020-08-26 15:23:35 +0530

    made changes in animation timings

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
+window.onload = function () { 
+  moveBackground();
+ }
+  
+  
   function moveBackground(){
     let pos = 0;
     let bg = document.getElementById("water");
-    let fxnId = setInterval(bgmove, 1);
-    function  bgmove(){
-      pos += (100);
+    setInterval( function(){
+      pos += 100;
       bg.style.backgroundPositionX = pos + "px";
-    }
+    },0);
   }
   
 
 var boat = document.querySelector("#boat");
-  setTimeout(function() {
+ setTimeout(function() {
     boat.style.transform = "translateX(1550px)";
-  }, 200);
+  }, 20);
+  
+
 
   var lotus1 = document.querySelector("#lotus1");
   setTimeout(function() {
     lotus1.style.transform = "translateX(-1500px)";
-  }, 200);
+  }, 20);
 
   var lotus2 = document.querySelector("#lotus2");
   setTimeout(function() {
     lotus2.style.transform = "translateX(-1200px)";
-  }, 200);
+  }, 20);
 
   var lily1 = document.querySelector("#lily1");
   setTimeout(function() {
     lily1.style.transform = "translateX(-1500px)";
-  }, 200);
+  }, 20);
 
-  var lily21 = document.querySelector("#lily2");
+  var lily2 = document.querySelector("#lily2");
   setTimeout(function() {
     lily2.style.transform = "translateX(-1500px)";
-  }, 200);
+  }, 20);
